71eda140b4bf28bf6dc80506b99313818ebfb44b
askyr1m <akatokikwok@outlook.com>
2021-07-21 23:31:16 +0800

    [feat 384-396]: 新增BlendDemo工程:使用透明混合绘制水,并通过clip测试来渲染铁丝网盒; 混合因子; 混合与深度; 像素裁剪与alpha测试;
    混合技术:把当前要光栅化的像素和之前已光栅化到后台缓存的像素相融合,用以渲染水和玻璃半透明物体
    =============================
    blend的二元运算符的效果由D3D12_BLEND_OP枚举表示
    =============================
    混合因子P387
    =============================
    D3D12_BLEND_DESC{
    alpha-to-coverage功能,用以渲染叶片的多重采样技术
    IndependentBlendEnable最多可支持同时渲染8个渲染目标
    RenderTargets:具有8个D3D12_RENDER_TAGET_BLEND_DESC元素的数组,若设为false,则所有渲染目标默认使用RenderTarget[0]进行混合计算
    }
    =============================
    D3D12_RENDER_TARGET_BLEND_DESC 详见p390
    =============================
    绘制顺序:
    首先绘制无需混合的普通物体,接着,再根据混合物体和摄像机的距离由远及近通过混合的方式来绘制它们
    原因是要先将透明物体后侧的所有像素都预写入后台缓存
    ===============================
    如果要让S物体集合中的物体进行加法混合,如果同时禁用了深度写入,S里的物体前一个就不会因为深度写入而盖住后一个物体,但是深度值读取和深度测试仍然是开启的,会导致非混合物体仍然会遮挡更深的混合几何体
    ==================================

---
+++ Chapter 10 Blending/BlendDemo/GeometryGenerator.h
@@ -0,0 +1,123 @@
+﻿//***************************************************************************************
+// GeometryGenerator.h by Frank Luna (C) 2011 All Rights Reserved.
+//   
+// Defines a static class for procedurally generating the geometry of 
+// common mathematical objects.
+//
+// All triangles are generated "outward" facing.  If you want "inward" 
+// facing triangles (for example, if you want to place the camera inside
+// a sphere to simulate a sky), you will need to:
+//   1. Change the Direct3D cull mode or manually reverse the winding order.
+//   2. Invert the normal.
+//   3. Update the texture coordinates and tangent vectors.
+//***************************************************************************************
+
+#pragma once
+
+#include <cstdint>
+#include <DirectXMath.h>
+#include <vector>
+
+/* 用户提供参数以自动生成的几何体存入GeometryGenerator类里
+* 它是一个工具类,用于生成栅格,球体,柱体,长方体
+* 此类还可以创建出后续技术要使用的顶点数据,然后存到顶点缓存里*/
+class GeometryGenerator
+{
+public:
+
+	using uint16 = std::uint16_t;
+	using uint32 = std::uint32_t;
+
+	/* GeometryGenerator::Vertex型 结构体, 带有Postion, Normal, Tangent, uv等构造器*/
+	struct Vertex
+	{
+		Vertex() {}
+		Vertex(
+			const DirectX::XMFLOAT3& p,
+			const DirectX::XMFLOAT3& n,
+			const DirectX::XMFLOAT3& t,
+			const DirectX::XMFLOAT2& uv) :
+			Position(p),
+			Normal(n),
+			TangentU(t),
+			TexC(uv) {}
+		Vertex(
+			float px, float py, float pz,
+			float nx, float ny, float nz,
+			float tx, float ty, float tz,
+			float u, float v) :
+			Position(px, py, pz),
+			Normal(nx, ny, nz),
+			TangentU(tx, ty, tz),
+			TexC(u, v) {}
+
+		DirectX::XMFLOAT3 Position;
+		DirectX::XMFLOAT3 Normal;
+		DirectX::XMFLOAT3 TangentU;
+		DirectX::XMFLOAT2 TexC;
+	};
+
+	/* MeshData是一个嵌套在GeometryGenerator类里用于存储顶点\索引表的简易结构体
+	* MeshData结构体存有顶点数组\索引数组
+	* 此结构体通常被拿来构造 模型几何体
+	*/
+	struct MeshData
+	{
+		std::vector<Vertex> Vertices;// MeshData结构体里的 顶点数组
+		std::vector<uint32> Indices32;// MeshData结构体里的 索引数组
+
+		/* 构建1个uint16型的索引数组*/
+		std::vector<uint16>& GetIndices16()
+		{
+			if (mIndices16.empty()) 			{
+				mIndices16.resize(Indices32.size());
+				for (size_t i = 0; i < Indices32.size(); ++i)
+					mIndices16[i] = static_cast<uint16>(Indices32[i]);
+			}
+
+			return mIndices16;
+		}
+
+	private:
+		std::vector<uint16> mIndices16;
+	};
+
+	///<summary>
+	/// Creates a box centered at the origin with the given dimensions, where each
+	/// face has m rows and n columns of vertices.
+	///</summary>
+	MeshData CreateBox(float width, float height, float depth, uint32 numSubdivisions);
+
+	///<summary>
+	/// Creates a sphere centered at the origin with the given radius.  The
+	/// slices and stacks parameters control the degree of tessellation.
+	///</summary>
+	MeshData CreateSphere(float radius, uint32 sliceCount, uint32 stackCount);
+
+	///<summary>
+	/// Creates a geosphere centered at the origin with the given radius.  The
+	/// depth controls the level of tessellation.
+	///</summary>
+	MeshData CreateGeosphere(float radius, uint32 numSubdivisions);
+
+
+	/// 生成圆台的基本思路是遍历每个环, 并生成位于环上的各个顶点
+	MeshData CreateCylinder(float bottomRadius, float topRadius, float height, uint32 sliceCount,/*切片数量*/ uint32 stackCount/*层数*/);
+
+	
+	/// 创建栅格,用来绘制水波荡漾
+	/// 待栅格创建后,可以从MeshData里获取所需顶点, 根据顶点的高度(即y坐标)把平坦的栅格变为表现山峰起伏的曲面
+	MeshData CreateGrid(float width, float depth, uint32 m, uint32 n);
+
+	///<summary>
+	/// Creates a quad aligned with the screen.  This is useful for postprocessing and screen effects.
+	///</summary>
+	MeshData CreateQuad(float x, float y, float w, float h, float depth);
+
+private:
+	void Subdivide(MeshData& meshData);
+	Vertex MidPoint(const Vertex& v0, const Vertex& v1);
+	void BuildCylinderTopCap(float bottomRadius, float topRadius, float height, uint32 sliceCount, uint32 stackCount, MeshData& meshData);
+	void BuildCylinderBottomCap(float bottomRadius, float topRadius, float height, uint32 sliceCount, uint32 stackCount, MeshData& meshData);
+};
+
---
+++ Chapter 10 Blending/BlendDemo/Shaders/Default.hlsl
@@ -0,0 +1,160 @@
+﻿//***************************************************************************************
+// Default.hlsl by Frank Luna (C) 2015 All Rights Reserved.
+//
+// Default shader, currently supports lighting.
+//***************************************************************************************
+
+// Defaults for number of lights.
+#ifndef NUM_DIR_LIGHTS
+    #define NUM_DIR_LIGHTS 3
+#endif
+
+#ifndef NUM_POINT_LIGHTS
+    #define NUM_POINT_LIGHTS 0
+#endif
+
+#ifndef NUM_SPOT_LIGHTS
+    #define NUM_SPOT_LIGHTS 0
+#endif
+
+// Include structures and functions for lighting.
+#include "LightingUtil.hlsl"
+
+Texture2D    gDiffuseMap : register(t0);
+
+
+SamplerState gsamPointWrap        : register(s0);
+SamplerState gsamPointClamp       : register(s1);
+SamplerState gsamLinearWrap       : register(s2);
+SamplerState gsamLinearClamp      : register(s3);
+SamplerState gsamAnisotropicWrap  : register(s4);
+SamplerState gsamAnisotropicClamp : register(s5);
+
+// Constant data that varies per frame.
+cbuffer cbPerObject : register(b0)
+{
+    float4x4 gWorld;
+	float4x4 gTexTransform;
+};
+
+// Constant data that varies per pass.
+cbuffer cbPass : register(b1)
+{
+    float4x4 gView;
+    float4x4 gInvView;
+    float4x4 gProj;
+    float4x4 gInvProj;
+    float4x4 gViewProj;
+    float4x4 gInvViewProj;
+    float3 gEyePosW;
+    float cbPerObjectPad1;
+    float2 gRenderTargetSize;
+    float2 gInvRenderTargetSize;
+    float gNearZ;
+    float gFarZ;
+    float gTotalTime;
+    float gDeltaTime;
+    float4 gAmbientLight;
+
+	// Allow application to change fog parameters once per frame.
+	// For example, we may only use fog for certain times of day.
+	float4 gFogColor;
+	float gFogStart;
+	float gFogRange;
+	float2 cbPerObjectPad2;
+
+    // Indices [0, NUM_DIR_LIGHTS) are directional lights;
+    // indices [NUM_DIR_LIGHTS, NUM_DIR_LIGHTS+NUM_POINT_LIGHTS) are point lights;
+    // indices [NUM_DIR_LIGHTS+NUM_POINT_LIGHTS, NUM_DIR_LIGHTS+NUM_POINT_LIGHT+NUM_SPOT_LIGHTS)
+    // are spot lights for a maximum of MaxLights per object.
+    Light gLights[MaxLights];
+};
+
+cbuffer cbMaterial : register(b2)
+{
+	float4   gDiffuseAlbedo;
+    float3   gFresnelR0;
+    float    gRoughness;
+	float4x4 gMatTransform;
+};
+
+struct VertexIn
+{
+	float3 PosL    : POSITION;
+    float3 NormalL : NORMAL;
+	float2 TexC    : TEXCOORD;
+};
+
+struct VertexOut
+{
+	float4 PosH    : SV_POSITION;
+    float3 PosW    : POSITION;
+    float3 NormalW : NORMAL;
+	float2 TexC    : TEXCOORD;
+};
+
+VertexOut VS(VertexIn vin)
+{
+	VertexOut vout = (VertexOut)0.0f;
+	
+    // Transform to world space.
+    float4 posW = mul(float4(vin.PosL, 1.0f), gWorld);
+    vout.PosW = posW.xyz;
+
+    // Assumes nonuniform scaling; otherwise, need to use inverse-transpose of world matrix.
+    vout.NormalW = mul(vin.NormalL, (float3x3)gWorld);
+
+    // Transform to homogeneous clip space.
+    vout.PosH = mul(posW, gViewProj);
+	
+	// Output vertex attributes for interpolation across triangle.
+	float4 texC = mul(float4(vin.TexC, 0.0f, 1.0f), gTexTransform);
+	vout.TexC = mul(texC, gMatTransform).xy;
+
+    return vout;
+}
+
+float4 PS(VertexOut pin) : SV_Target
+{
+    // 漫反照率 == 从这张2D纹理里提取此像素的漫反射率 并与 材质常量里的反照率 相乘
+    float4 diffuseAlbedo = gDiffuseMap.Sample(gsamAnisotropicWrap, pin.TexC) * gDiffuseAlbedo;
+	
+    // 注意,alpha测试的开销巨大,只有在必要情况下才使用
+#ifdef ALPHA_TEST
+    // 项目里使用的是铁丝网贴图,alpha通道作用就是从纹理里屏蔽非铁丝网部分
+	// 采集像素里的alpha分量,如果此值极小并接近0,则暗示此像素是完全透明,建议把此像素裁减掉,以免后续再处理它
+    // 不需要接近完全透明的像素,丢弃掉
+	clip(diffuseAlbedo.a - 0.1f);
+#endif
+
+    // Interpolating normal can unnormalize it, so renormalize it.
+    pin.NormalW = normalize(pin.NormalW);
+
+    // Vector from point being lit to eye. 
+	float3 toEyeW = gEyePosW - pin.PosW;
+	float distToEye = length(toEyeW);
+	toEyeW /= distToEye; // normalize
+
+    // Light terms.
+    float4 ambient = gAmbientLight*diffuseAlbedo;
+
+    const float shininess = 1.0f - gRoughness;
+    Material mat = { diffuseAlbedo, gFresnelR0, shininess };
+    float3 shadowFactor = 1.0f;
+    float4 directLight = ComputeLighting(gLights, mat, pin.PosW,
+        pin.NormalW, toEyeW, shadowFactor);
+
+    float4 litColor = ambient + directLight;
+
+#ifdef FOG
+	float fogAmount = saturate((distToEye - gFogStart) / gFogRange);
+	litColor = lerp(litColor, gFogColor, fogAmount);
+#endif
+
+    // 从之前计算好的漫反照率里获取alpha值
+    litColor.a = diffuseAlbedo.a;
+
+    return litColor;
+}
+
+
